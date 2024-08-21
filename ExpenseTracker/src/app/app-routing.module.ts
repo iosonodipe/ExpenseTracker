11e27d53045eab1a115579c491445169cbc342a7
iosonodipe <dipecod@gmail.com>
2024-08-21 17:33:28 +0200

    moduli

--- ExpenseTracker/src/app/app-routing.module.ts
+++ ExpenseTracker/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'transactions', loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsModule) }, { path: 'pages/transactions', loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsModule) }, { path: 'transactions', loadChildren: () => import('./pages/transactions/transactions.module').then(m => m.TransactionsModule) }, { path: 'statement', loadChildren: () => import('./pages/statement/statement.module').then(m => m.StatementModule) }, { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ExpenseTracker/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  expensesUrl: 'http://localhost:3000/expenses',
+  accountsUrl: 'http://localhost:3000/accounts',
+  causalsUrl: 'http://localhost:3000/causals'
+};
